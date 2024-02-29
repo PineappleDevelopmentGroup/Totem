29f30a3eed97f205f4085aab5548cfcb042b1b50
Miles Holder <mwholder2005@gmail.com>
2024-02-29 08:57:53 -0600

    fix totems work for all death types

--- totem-plugin/src/main/kotlin/sh/miles/totem/listener/EntityDamageListener.kt
+++ totem-plugin/src/main/kotlin/sh/miles/totem/listener/EntityDamageListener.kt
@@ -1,18 +1,21 @@
 package sh.miles.totem.listener
 
-import org.bukkit.EntityEffect
 import org.bukkit.Material
 import org.bukkit.entity.LivingEntity
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.EntityResurrectEvent
 import org.bukkit.persistence.PersistentDataType
 import sh.miles.totem.api.impl.TotemItemImpl
 import sh.miles.totem.registry.TotemItemRegistry
 import sh.miles.totem.util.TotemInventoryUtils
+import java.util.UUID
 
 @Suppress("UnstableApiUsage")
 class EntityDamageListener : Listener {
+
+    private val doNotRevive = mutableSetOf<UUID>()
 
     @EventHandler
     fun onEntityTakeDamage(event: EntityDamageEvent) {
@@ -23,22 +26,19 @@ class EntityDamageListener : Listener {
 
         val equipment = entity.equipment ?: return
         val damageType = event.damageSource.damageType
-        val firstTotem = TotemInventoryUtils.getEquipmentSlotsWithTarget(Material.TOTEM_OF_UNDYING, equipment)
-            .stream()
-            .filter {
-                val meta = equipment.getItem(it).itemMeta!!
-                return@filter meta.persistentDataContainer.has(TotemItemImpl.ITEM_KEY)
-            }.map {
-                val key = equipment.getItem(it).itemMeta!!.persistentDataContainer.get(
-                    TotemItemImpl.ITEM_KEY,
-                    PersistentDataType.STRING
-                )!!
-                return@map Pair(it, TotemItemRegistry.get(key).orElseThrow())
-            }.filter { it.second.settings.blockTypes.contains(damageType) }
-            .findFirst()
+        val firstTotem =
+            TotemInventoryUtils.getEquipmentSlotsWithTarget(Material.TOTEM_OF_UNDYING, equipment).stream().filter {
+                    val meta = equipment.getItem(it).itemMeta!!
+                    return@filter meta.persistentDataContainer.has(TotemItemImpl.ITEM_KEY)
+                }.map {
+                    val key = equipment.getItem(it).itemMeta!!.persistentDataContainer.get(
+                        TotemItemImpl.ITEM_KEY, PersistentDataType.STRING
+                    )!!
+                    return@map Pair(it, TotemItemRegistry.get(key).orElseThrow())
+                }.filter { it.second.settings.blockTypes.contains(damageType) }.findFirst()
 
         // TODO: refactor. Maybe add trigger to API?
-        firstTotem.ifPresent {
+        firstTotem.ifPresentOrElse({
             val totem = it.second
             if (totem is TotemItemImpl) {
                 totem.trigger(entity, it.first, true)
@@ -48,6 +48,16 @@ class EntityDamageListener : Listener {
             val source = event.damageSource
             val yaw = if (source.damageLocation != null) source.damageLocation!!.yaw else 0.0F
             entity.playHurtAnimation(yaw)
+        }, {
+            doNotRevive.add(entity.uniqueId)
+        })
+    }
+
+    @EventHandler
+    fun onResurrect(event: EntityResurrectEvent) {
+        if (doNotRevive.contains(event.entity.uniqueId)) {
+            doNotRevive.remove(event.entity.uniqueId)
+            event.isCancelled = true
         }
     }
 
